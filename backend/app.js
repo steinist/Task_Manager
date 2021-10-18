416c079f460496d908d1a305b1135636a09f8905
Steven Indrayana <steven.indrayana.istimur@gmail.com>
2021-10-19 02:37:43 +0800

    utilize api

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 const app = express();
 
 import {router as tasks} from './routes/tasks.js'
@@ -11,6 +12,7 @@ dotenv.config()
 
 // middleware
 app.use(express.json())
+app.use(cors())
 // app.use(express.static('./public')) //serve static file (React part !!!)
 
 app.use('/api/v1/tasks', tasks)
